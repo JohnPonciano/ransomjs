f6731975003f49cb759fd91530be936e82a9700f
johnponciano <jhow.ponciano@hotmail.com>
2021-11-05 11:43:52 -0300

    Created Extensions list

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const extensions = require ('./extensions')
+
+
+
+const devConfig = {
+
+    remoteServer: 'http://localhost:3333' ,
+    extensions,
+    
+}
+
+module.exports = devConfigd
---
+++ config/extensions.js
@@ -0,0 +1,3 @@
+module.exports =[
+    'exe','mp3','mp4','pdf','doc','docx'
+]
